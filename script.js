3f7ae3b88456293a46ae7e3887b77ac1a45ed2aa
joaovsantoscarvalho <carvalho.joao08@escola.pr.gov.br>
2026-05-25 09:23:31 -0300

    .

--- script.js
+++ script.js
@@ -56,14 +56,28 @@ function respostaSelecionada(opcaoSelecionada){
 
 function mostraResultado(){
     textoResultado.textContent = historiaFinal;
-    caixaPerguntas.textContent = "Resultado";
     caixaAlternativas.textContent = "";
     // chame a função podiumMedalhas aqui
-    
+    podiumMedalhas()
 }
 
 function podiumMedalhas(){
-    caixaPrincipal.style.backgroundImage = "url('img/bronze.png')";
+    if (pontos === 3) {
+        caixaPrincipal.style.backgroundImage = "url('img/bronze.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 3 pontos é BRONZE!";
+    }
+    else if (pontos === 4) {
+        caixaPrincipal.style.backgroundImage = "url('img/prata.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 4 pontos é PRATA!";
+    }
+    else if (pontos === 5) {
+        caixaPrincipal.style.backgroundImage = "url('img/ouro.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 5 pontos é OURO!";
+    }
+    else if (pontos < 3) {
+        caixaPrincipal.style.backgroundImage = "url('img/perdeu.png')";
+        caixaPerguntas.textContent = "Resultado da competição: PERDEU!";
+    }
 }
 
 //crie uma função podiumMedalhas
